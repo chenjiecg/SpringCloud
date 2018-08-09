5d6ec7217c6a4d4da9c7c3b9be2ea7cd218e88f1
chenjie <chenjiecg506@163.com>
2018-08-09 11:45:57 +0800

    配置中心服务端

---
+++ springcloud-config/config-server/src/main/java/com/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 
+ * @author xiaoH
+ * @date 2018年8月9日
+ * @Description
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
