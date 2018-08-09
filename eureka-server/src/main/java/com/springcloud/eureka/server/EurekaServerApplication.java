a756c61f854cb83ed14492faa1f66e07ac0f0897
chenjie <chenjiecg506@163.com>
2018-08-09 11:34:16 +0800

    eureka服务注册中心

---
+++ eureka-server/src/main/java/com/springcloud/eureka/server/EurekaServerApplication.java
@@ -0,0 +1,21 @@
+package com.springcloud.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author xiaoH
+ * @date 2018年8月7日
+ * @Description
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
