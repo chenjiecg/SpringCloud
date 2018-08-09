fb84cc15412bf6b227dd1d474fdaf056e1197ed5
chenjie <chenjiecg506@163.com>
2018-08-09 15:27:58 +0800

    config client端

---
+++ springcloud-config/config-client/src/main/java/com/springcloud/config/ConfigClientApplication.java
@@ -0,0 +1,19 @@
+package com.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author xiaoH
+ * @date 2018年8月9日
+ * @Description
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+}
